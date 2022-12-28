6ba0d7c77d44bf57d275b3ddcd2441cdee84088f
Brian Perry <contact.brianperry@gmail.com>
2022-12-28 16:41:54 -0600

    Add initial framework prompt

--- packages/create-sanctuary/bin/create-sanctuary.mjs
+++ packages/create-sanctuary/bin/create-sanctuary.mjs
@@ -2,6 +2,7 @@
 
 import boxen from 'boxen';
 import chalk from 'chalk';
+import prompts from 'prompts';
 
 import child_process from 'child_process';
 
@@ -14,4 +15,26 @@ console.log("\nFirst, we'll shuttle you over to the create-astro cli to create y
 // TODO - Ensure we sanitize input
 child_process.spawnSync("npm create astro@latest drupal-sanctuary", { encoding : 'utf8', stdio: 'inherit', shell: true });
 
-console.log( "=== After Spawning Process ===" );
+console.log("\nNext, we'll guide you through optionally selecting Astro integrations to add functionality to your project...\n");
+
+console.log(boxen('Integrations are optional, and can be added later with `astro add`', {title: 'Protip!', titleAlignment: 'center', padding: 1 }) + '\n');
+
+(async () => {
+    const response = await prompts({
+        type: 'multiselect',
+        name: 'frameworks',
+        message: 'Pick one or more frameworks',
+        instructons: false, // TODO - can we remove redundant instructions?
+        choices: [
+          { title: 'React', value: 'react' },
+          { title: 'Vue', value: 'vue' },
+          { title: 'Svelte', value: 'svelte' },
+          { title: 'Lit', value: 'lit' }
+        ],
+        hint: '- Space to select. Return to submit'
+    });
+  
+    console.log(response);
+  })();
+
+  // TODO - run Astro add here, ideally non-interactively.
