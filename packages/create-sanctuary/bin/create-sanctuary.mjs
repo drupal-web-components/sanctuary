386bb0ac548a0faa18b609325b166143166f81de
Brian Perry <contact.brianperry@gmail.com>
2023-01-16 13:59:15 -0600

    Install integrations via plop

--- packages/create-sanctuary/bin/create-sanctuary.mjs
+++ packages/create-sanctuary/bin/create-sanctuary.mjs
@@ -14,8 +14,8 @@ const args = yargs(hideBin(process.argv))
     describe: 'directory where your project will be created'
   })
   .parse()
-console.log('Args:', args);
-const directory = args['_'].slice(0,1).shift();
+
+let directory = args['_'].slice(0,1).shift();
 
 // Let us introduce ourselves.
 console.log(chalk.blue(boxen('An enjoyable project scaffolder for decoupled Drupal explorers. Powered by Astro.', {title: 'Welcome to Drupal Sanctuary!', titleAlignment: 'center', padding: 1, margin: 1, borderStyle: 'round'})));
@@ -23,54 +23,38 @@ console.log(chalk.blue(boxen('An enjoyable project scaffolder for decoupled Drup
 // Create an instance of plop and load the generators we'll be using
 const plop = await nodePlop(`plopfile.mjs`);
 const astro = plop.getGenerator('astro');
+const integrations = plop.getGenerator('integrations');
 
 // Create an Astro project in the specified directory
 if (directory) {
   checkDir(directory);
-  astro.runActions({directory}).then(function (results) {
-    console.log('Action results with directory arg', results);
-  });
+  await astro.runActions({directory});
 }
 else {
-  astro.runPrompts().then(function (results) {
-    console.log('Prompt Results', results);
-    astro.runActions({directory: results.directory}).then(function (results) {
-      console.log('Action results', results);
-    });
+  await astro.runPrompts().then(async function (results) {
+    directory = results.directory;
+    await astro.runActions({directory});
   });
 }
 
-// console.log(chalk.blue("\nNext, we'll guide you through optionally selecting Astro integrations to add functionality to your project...\n"));
+console.log(chalk.blue("\nNext, we'll guide you through optionally selecting Astro integrations to add functionality to your project..."));
+console.log(chalk.blue(boxen('Integrations are optional, and can be added later with `astro add`', {title: 'Protip!', titleAlignment: 'center', padding: 1, margin: 1, borderStyle: 'round' })));
 
-// console.log(boxen('Integrations are optional, and can be added later with `astro add`', {title: 'Protip!', titleAlignment: 'center', padding: 1 }) + '\n');
 
-// (async () => {
-//     const response = await prompts({
-//         type: 'multiselect',
-//         name: 'frameworks',
-//         message: 'Pick one or more frameworks',
-//         instructons: false, // TODO - can we remove redundant instructions?
-//         choices: [
-//           { title: 'React', value: 'react' },
-//           { title: 'Vue', value: 'vue' },
-//           { title: 'Svelte', value: 'svelte' },
-//           { title: 'Lit', value: 'lit' }
-//         ],
-//         hint: '- Space to select. Return to submit'
-//     });
-  
-//     // TODO - Make project directory path configurable
-//     if (response.frameworks.length) {
-//         child_process.spawnSync(`cd drupal-sanctuary && npx astro add ${response.frameworks.join(' ')} -y`, { encoding : 'utf8', stdio: 'inherit', shell: true });
-//     }
-//   })();
+await integrations.runPrompts().then(async function (results) {
+  await integrations.runActions({directory: directory, frameworks: results.frameworks});
+});
+
+console.log(chalk.blue("\nWelcome to space. Enjoy your new project!\n"));
 
 /**
  * Next:
+ * Add flags to allow non-interactive runs
  * Other integrations
  * SSR options
  * Drupal data fetching
  * Prompt for envars
  * Other Drupal related integrations
  * Drupal module configurations
+ * Bug - breaking out of first prompt doesn't break out of script
  */
--- packages/create-sanctuary/plopfile.mjs
+++ packages/create-sanctuary/plopfile.mjs
@@ -7,21 +7,31 @@ export default function (plop) {
     plop.setActionType('initAstro', function (answers) {
         return new Promise((resolve, reject) => {
             if (answers.directory) {
-                console.log('Prompt answers', answers);
                 checkDir(answers.directory);
-                console.log(chalk.blue("First, we'll shuttle you over to the create-astro cli to create your initial project..."));
+                console.log(chalk.blue("\nFirst, we'll shuttle you over to the create-astro cli to create your initial project..."));
                 // TODO - sanitize input
                 child_process.spawnSync(`npm create astro@latest ${answers.directory}`, { encoding : 'utf8', stdio: 'inherit', shell: true });
-                resolve('Welcome to space. Now let\'s explore.');
+                resolve('Project created.');
             } else {
                 reject('Unknown project directory.');
+            }
+        });
+    });
+
+    plop.setActionType('addIntegrations', function (answers) {
+        return new Promise((resolve, reject) => {
+            if (answers.frameworks) {
+                child_process.spawnSync(`cd ${answers.directory} && npx astro add ${answers.frameworks.join(' ')} -y`, { encoding : 'utf8', stdio: 'inherit', shell: true });
+                resolve('Integrations installed.');
+            } else {
+                reject('Unknown integrations.');
             }
         });
     });
     
     // Astro project generator
     plop.setGenerator('astro', {
-        description: 'create an astro project',
+        description: 'create an Astro project',
         prompts: [{
             type: 'input',
             name: 'directory',
@@ -30,6 +40,23 @@ export default function (plop) {
         }],
         actions: [{
             type: 'initAstro',
+        }]
+    });
+    plop.setGenerator('integrations', {
+        description: 'add Astro integrations',
+        prompts: [{
+            type: 'checkbox',
+            name: 'frameworks',
+            message: 'Optionally, select one or more frameworks',
+            choices: [
+                { name: 'React', value: 'react' },
+                { name: 'Vue', value: 'vue' },
+                { name: 'Svelte', value: 'svelte'},
+                { name: 'Lit', value: 'lit'},
+            ],
+        }],
+        actions: [{
+            type: 'addIntegrations',
         }]
     });
 };
