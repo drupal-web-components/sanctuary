42edd1b54bf24c3444c23c2d2e37a6cf95905d25
Brian Perry <contact.brianperry@gmail.com>
2023-02-04 15:41:26 -0600

    Rebuild astro-drupal-state example using latest create app

--- examples/astro-drupal-state/astro.config.mjs
+++ examples/astro-drupal-state/astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import astroDrupalState from '@gdwc/astro-drupal-state'
 
 // https://astro.build/config
-export default defineConfig({
-    integrations: [astroDrupalState()]
-});
+export default defineConfig({});
